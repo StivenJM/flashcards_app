24b1f264c9b47c789e7e66bf98f1c41cd9431609
WiloG2 <fexbius@gmail.com>
2025-06-12 09:17:24 -0500

    Router modificado

--- lib/routing/router.dart
+++ lib/routing/router.dart
@@ -6,6 +6,11 @@ import '../ui/home/view_models/home_viewmodel.dart';
 import '../ui/home/widgets/home_screen.dart';
 import 'routes.dart';
 
+import '../ui/settings/view_models/settings_viewmodel.dart';
+import '../ui/statistics/view_models/statistics_viewmodel.dart';
+import '../ui/settings/widgets/settings_screen.dart';
+import '../ui/statistics/widgets/statistics_screen.dart';
+
 /// Top go_router entry point.
 GoRouter router() => GoRouter(
   initialLocation: Routes.home,
@@ -14,12 +19,15 @@ GoRouter router() => GoRouter(
     GoRoute(
       path: Routes.home,
       builder: (context, state) {
-        final viewModel = HomeViewModel(
-          categoryRepository: context.read()
-        );
+        final viewModel = HomeViewModel(categoryRepository: context.read());
         return HomeScreen(viewModel: viewModel);
       },
       routes: [],
+    ),
+    GoRoute(path: '/settings', builder: (context, state) => SettingsScreen()),
+    GoRoute(
+      path: '/statistics',
+      builder: (context, state) => const StatisticsScreen(),
     ),
   ],
 );
